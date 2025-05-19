6c81fe2d5eb4ada808df86713e86ea23eeccee26
SeungYeop <syt05342@naver.com>
2025-05-19 12:32:14 +0900

    fix: 포트 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
+    port: 3000, // ✅ 포트 지정 (기본은 5173, 이걸 원하는 값으로 바꿔줌)
     proxy: {
-      '/apt': {
-        target: 'http://localhost:8080', // Spring Boot 서버 주소
+      "/apt": {
+        target: "http://localhost:8080",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apt/, '/apt'), // 경로 유지
+        rewrite: (path) => path.replace(/^\/apt/, "/apt"),
       },
     },
   },
-})
+});
