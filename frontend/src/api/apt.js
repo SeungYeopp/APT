3b83f2d867c430ab528cced14fc8de4aa6349523
SeungYeop <syt05342@naver.com>
2025-05-26 09:30:38 +0900

    fix

--- frontend/src/api/apt.js
+++ frontend/src/api/apt.js
@@ -2,37 +2,27 @@ import axios from "@/util/axios-common";
 
 function getAptListByArea(location, success, fail) {
   let url = "";
-  url = `${import.meta.env.VITE_VUE_API_URL}/apt/area/${location}`;
+  url = `/apt/area/${location}`;
   axios.get(url).then(success).catch(fail);
 }
 
 // 아파트 이름으로 리스트 조회
 function getAptListByKeyword(keyword, success, fail) {
-  axios
-    .get(`${import.meta.env.VITE_VUE_API_URL}/apt/name/${keyword}`)
-    .then(success)
-    .catch(fail);
+  axios.get(`/apt/name/${keyword}`).then(success).catch(fail);
 }
 
 // 특정 아파트 거래 정보 조회
 function getAptDeal(aptSeq, success, fail) {
-  axios
-    .get(`${import.meta.env.VITE_VUE_API_URL}/apt/housedeal/${aptSeq}`)
-    .then(success)
-    .catch(fail);
+  axios.get(`/apt/housedeal/${aptSeq}`).then(success).catch(fail);
 }
 
 function getDongCodeByName(keyword, success) {
-  axios
-    .get(`${import.meta.env.VITE_VUE_API_URL}/apt/dong/${keyword}`)
-    .then(success);
+  axios.get(`/apt/dong/${keyword}`).then(success);
 }
 
 function getDongCodeByDetails(sidoName, gugunName, dongName, success, fail) {
   axios
-    .get("${import.meta.env.VITE_VUE_API_URL}/apt/code", {
-      params: { sidoName, gugunName, dongName },
-    })
+    .get("/apt/code", { params: { sidoName, gugunName, dongName } })
     .then(success)
     .catch(fail);
 }
--- frontend/src/util/axios-common.js
+++ frontend/src/util/axios-common.js
@@ -4,6 +4,7 @@ const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_VUE_API_URL}`, // 백엔드 서버 주소
   headers: {
     "Content-Type": "application/json",
+    withCredentials: true,
   },
 });
 
